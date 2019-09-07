c0c53dd68a534f3f6555e5a12e6a60e29ab79461
Rajesh Danabal <rajeshdanabal@gmail.com>
2019-09-07 19:44:18 +0530

    Client - Question Templates integration

---
+++ client/src/QuestionTemplate/QuestionTemplate.js
@@ -0,0 +1,159 @@
+import React, { useState } from 'react';
+import {
+    FormControl,
+    InputLabel,
+    Input,
+    FormHelperText,
+    Button,
+    Container,
+    Typography,
+    CssBaseline,
+    TextField,
+    makeStyles,
+    MenuItem,
+    Select,
+    OutlinedInput
+} from '@material-ui/core';
+import Table from '../Others/TableComponent';
+
+function QuestionTemplate() {
+    const [state, setState] = useState({
+        question: '',
+        questionTypeHandler: 'TextField',
+
+    })
+    const [stateQ, setStateQ] = useState({
+        questions: []
+    });
+    const [stateRender, setStateRender] = useState({
+        addQuestionJSX: ''
+    });
+    const [stateOpt, setStateOpt] = useState({
+        questionTypeValueHandler: ['']
+    })
+
+    const useStyles = makeStyles(theme => ({
+        button: {
+            margin: theme.spacing(1),
+        },
+        input: {
+            display: 'none',
+        },
+    }));
+
+    const classes = useStyles();
+    let addQuestionJSX;
+    let formSubmit = (event) => {
+        stateQ.questions.push({
+            question: state.question,
+            questionTypeHandler: state.questionTypeHandler,
+            questionTypeValueHandler: stateOpt.questionTypeValueHandler
+        });
+
+        setState({
+            question: '',
+            questionTypeHandler: 'TextField',
+
+        })
+        state.questionTypeHandler = 'TextField'
+        stateOpt.questionTypeValueHandler = ['']
+        questionTypeJSX();
+    }
+    let questionHandler = (event) => {
+        state.question = event.target.value;
+        setState({
+            question: state.question,
+            questionTypeHandler: state.questionTypeHandler,
+
+        })
+    }
+    let questionTypeHandler = (event) => {
+        state.questionTypeHandler = event.target.value;
+        questionTypeJSX();
+    }
+    let questionTypeValueHandler = (event, index) => {
+        let temp = stateOpt.questionTypeValueHandler;
+        temp[index] = event.target.value;
+        setStateOpt({
+            questionTypeValueHandler: temp
+        });
+    }
+    let addButtonHandler = (event, index) => {
+        let temp = Object.assign(stateOpt.questionTypeValueHandler);
+        temp.push('');
+
+        setStateOpt({
+            questionTypeValueHandler: temp
+        });
+        questionTypeJSX();
+    }
+    let deleteButton = (event, index) => {
+        let temp = Object.assign(stateOpt.questionTypeValueHandler);
+        temp.splice(index, 1);
+        stateOpt.questionTypeValueHandler = temp;
+        questionTypeJSX();
+        setStateOpt({
+            questionTypeValueHandler: temp
+        });
+    }
+    let questionTypeJSX = () => {
+        let temp;
+        if (state.questionTypeHandler === 'TextField') {
+            temp = <div></div>
+        } else {
+            temp = <Table value={stateOpt.questionTypeValueHandler} inputFieldHandler={questionTypeValueHandler} addButtonHandler={addButtonHandler} deleteButton={deleteButton}></Table>
+        }
+        setStateRender({
+            addQuestionJSX: temp
+        })
+    }
+    return (
+        <div>
+            <FormControl fullWidth="true" style={{ padding: "20px" }}>
+                <TextField
+                    id="outlined-full-width"
+                    label="Question:"
+                    style={{ padding: "10px" }}
+                    fullWidth
+                    variant="outlined"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    value={state.question}
+                    onChange={questionHandler}
+                />
+                <Select
+                    native
+                    style={{ margin: " 10px" }}
+                    value={state.questionTypeHandler}
+                    placeholder="QuestionType"
+                    onChange={questionTypeHandler}
+                    input={<OutlinedInput name="age" id="filled-age-native-simple" />}
+                >
+                    <option value={'TextField'}>TextField</option>
+                    <option value={'Radio'}>Radio</option>
+                    <option value={'DropDown'}>DropDown</option>
+                    <option value={'checkBox'}>checkBox</option>
+                </Select>
+
+                {stateRender.addQuestionJSX}
+                <Button variant="contained" color="primary" className={classes.button} onClick={formSubmit}>
+                    Add Question
+                </Button>
+
+            </FormControl>
+
+            
+            {stateQ.questions.map((value, index) =>
+                <div>
+                    <label>{index}</label>
+                    <label>question:{value.question}</label>
+                    <label>questionType:{value.questionTypeHandler}</label>
+                    <label>questionTypeValue:{value.questionTypeValueHandler}</label>
+                </div>
+            )}
+        </div >
+    )
+}
+
+export default QuestionTemplate;
